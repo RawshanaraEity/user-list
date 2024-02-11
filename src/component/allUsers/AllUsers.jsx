260953fb80b395253fb28a61b74f7ad063e27ba3
Rawshanara Eity <rawshanara.eity@gmail.com>
2024-02-11 14:58:08 +0600

    [allusers]:added search and sort field

--- src/component/allUsers/AllUsers.jsx
+++ src/component/allUsers/AllUsers.jsx
@@ -1,22 +1,70 @@
+
+import { useState } from "react";
 import UserCard from "./UserCard";
 import useUsersData from "../../Hooks/useUsersData";
+import { CiSearch } from "react-icons/ci";
 
 const AllUsers = () => {
+    const [searchName, setSearchName] = useState("");
+    const [sortBy, setSortBy] = useState("");
 
-    const allUsers = useUsersData()
+    const allUsers = useUsersData();
 
+    const filteredUsers = allUsers?.filter(user =>
+        user?.firstName?.toLowerCase().includes(searchName.toLowerCase())
+    );
+
+    const sortedUsers = [...filteredUsers].sort((a, b) => {
+        if (sortBy === "firstName") {
+            return a.firstName.localeCompare(b.firstName);
+        } else if (sortBy === "email") {
+            return a.email.localeCompare(b.email);
+        } else if (sortBy === "company") {
+            return a.company.name.localeCompare(b.company.name);
+        }
+        return 0;
+    });
+
+    const handleSearchChange = e => {
+        setSearchName(e.target.value);
+    };
+
+    const handleSortChange = e => {
+        setSortBy(e.target.value);
+    };
 
     return (
-        <div className="py-10">
-            
-            {/* <h2 className="text-4xl"> All users: {allUsers.length} </h2> */}
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-5">
-           {
-                allUsers.map(user => <UserCard key={user.id} user={user}></UserCard>)
-            }
-           </div>
+        <div className="py-10 text-center">
+            <div className="mb-4">
+                <input
+                    type="text"
+                    placeholder="Search by name"
+                   
+                    value={searchName}
+                    onChange={handleSearchChange}
+                    className="px-4 py-2 text-lg border rounded-md rounded-r-none focus:outline-none focus:border-red-500"
+                />
+                <button className="btn mr-5 rounded-l-none bg-red-600" > <CiSearch className=" text-white font-bold"></CiSearch> </button>
+                <select
+                    value={sortBy}
+                    onChange={handleSortChange}
+                    className="ml-2 px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                >
+                    <option value="">Default</option>
+                    <option value="firstName">Name</option>
+                    <option value="email">Email</option>
+                    <option value="company">Company Name</option>
+                </select>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-5">
+                {sortedUsers.map(user => (
+                    <UserCard key={user.id} user={user} />
+                ))}
+            </div>
         </div>
     );
 };
 
 export default AllUsers;
+
+
--- src/component/allUsers/UserCard.jsx
+++ src/component/allUsers/UserCard.jsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import { CiLocationOn } from "react-icons/ci";
 
 const UserCard = ({ user }) => {
   const { id, image, firstName, lastName, email } = user;
 
   return (
     <div>
-      <div className="card bg-base-100  shadow-xl">
+      <div className="card bg-base-100 shadow-xl">
         <figure className="px-5 pt-10 ">
           <img src={image} alt="Image" className="h-48 rounded-xl " />
         </figure>
-        <div className="pl-10 pt-10 text-start h-48">
+        <div className="text-left pl-10 pt-10  h-48">
           <Link to={`userDetails/${id}`}>
             <h2 className="card-title mb-2 hover:text-red-600 hover:underline">
               Name: {firstName} {lastName}
@@ -18,9 +19,12 @@ const UserCard = ({ user }) => {
           <p>Email: {email}</p>
           <div className="card-actions gap-0 flex-col">
             <h2>Company: {user?.company?.name} </h2>
+           <div className="flex items-center gap-2">
+           <span className="text-xl"><CiLocationOn></CiLocationOn></span>
             <p>
-              Address: {user?.address.address},{user?.address?.city}{" "}
+               {user?.address.address},{user?.address?.city}
             </p>
+           </div>
           </div>
         </div>
       </div>
